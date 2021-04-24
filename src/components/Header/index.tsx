6499ee362b14d4b437dbffb3a1bbc2e695698012
Eduardo Luiz <hello@eduardoluiz.dev>
2021-04-24 17:42:53 -0300

    chore: create dinamic static internal episodes page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import format from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
 
@@ -10,7 +11,11 @@ export function Header() {
 
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr" />
+      <Link href={`/`}>
+        <a>
+          <img src="/logo.svg" alt="Podcastr" />
+        </a>
+      </Link>
 
       <p>O melhor para você ouvir, sempre</p>
 
